f259502aa89b8208d9f2d55d5f66cf1496a0f027
tooqk4u <tooqk4u@protonmail.com>
2022-02-12 17:45:16 -0500

    fixed html GET

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,25 +1,27 @@
-const router = require('express').Router();
-const path = require('path');
+// require dependencies
 const fs = require('fs');
+const path = require('path');
+const router = require('express').Router();
 const { notes } = require('../../db/db.json');
 const uniqid = require('uniqid');
 
-
+// get /api/notes reads db.json and returns all saved notes as json
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
+// post /api/notes receive a new note to save on the request and save to db.json then return new note to client
 router.post('/notes', (req, res) => {
-    
+    // set id based on what the next index of the array will be
     req.body.id = uniqid();
 
-    
+    // if any data if invalid, send 400 error
     if (!req.body.title || !req.body.text) {
-        res.status(400).send('Please enter title and text.');
+        res.status(400).send('The note is not properly formatted.');
     } else {
-        const newNote = req.body;
+        const note = req.body;
 
-        notes.push(newNote);
+        notes.push(note);
 
         fs.writeFileSync(
             path.join(__dirname, '../../db/db.json'),
@@ -30,5 +32,25 @@ router.post('/notes', (req, res) => {
     }
 });
 
+// delete /api/notes/:id to delete the selected note
+router.delete('/notes/:id', (req, res) => {
+    let id = req.params.id;
 
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === id) {
+            notes.splice(i, 1);
+            
+            fs.writeFileSync(
+                path.join(__dirname, '../../db/db.json'),
+                JSON.stringify({ notes: notes }, null, 2)
+            );
+
+            res.json(notes)
+            
+            return;
+        }
+    }
+});
+
+// export router
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,17 +1,20 @@
-const router = require("express").Router();
-const path = require("path");
+const path = require('path');
+const router = require('express').Router();
 
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/notes.html"))
-})
+// get / and return index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/index.html"))
-})
+// get /notes and return notes.html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
 
+// GET * returns index.html
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-
+// export router
 module.exports = router;
