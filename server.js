3f775ed00820db82a7d6274b4b6a7b4c47684c3d
tooqk4u <tooqk4u@protonmail.com>
2022-02-12 17:53:39 -0500

    added comments

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+// require dependencies
 const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-//const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
+// assign the port is present or default
+const PORT = process.env.PORT || 3001;
+
+// using express.js to create the server
+const app = express();
+
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
-//app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
+// run server on port
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
